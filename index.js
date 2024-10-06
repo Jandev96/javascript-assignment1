80a042c373e63ff22e996c26e20fb6fbbc26bd74
Jan Dev <jandev66@gmail.com>
2024-10-06 10:31:31 +0530

    initial index

---
+++ index.js
@@ -0,0 +1,8 @@
+//Write code in JavaScript to display all the even numbers till the number from 0 to 10. Output: 0 2 4 6 8 10 so it will display till 10 because 11 is an odd number
+let a=0;
+if(a%2===0){
+
+    for(a=0; a<=10; a=a+2){
+        console.log(a);
+    }
+}
